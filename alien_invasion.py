d72015d2de6b2775609829801b0c7e18dd6e88e4
Egi Mata <egi.mata1@gmail.com>
2020-06-18 00:53:34 +0200

    created bg color

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,14 +8,24 @@ def run_game():
     screen = pygame.display.set_mode((1400, 900))
     pygame.display.set_caption("Alien Invasion")
 
+    #background color
+    bg_color = (204, 204, 204)
+
     #main loop of the game
     while True:
 
         #keyboard&mouse
         for event in pygame.event.get():
+            screen.fill(bg_color)
+            #make screen visible
+            # pygame.display.flip()
             if event.type == pygame.QUIT:
                 sys.exit()
         
         pygame.display.flip()
 
+    
+    
 run_game()
+
+
