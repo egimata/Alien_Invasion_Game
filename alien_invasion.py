c5a296cec9982ba63805cf2180a4bd1c77098339
Egi Mata <egi.mata1@gmail.com>
2020-06-18 00:32:38 +0200

    Created Pygame Window

---
+++ alien_invasion.py
@@ -0,0 +1,21 @@
+import sys 
+
+import pygame
+
+def run_game():
+    #creating screen object
+    pygame.init()
+    screen = pygame.display.set_mode((1400, 900))
+    pygame.display.set_caption("Alien Invasion")
+
+    #main loop of the game
+    while True:
+
+        #keyboard&mouse
+        for event in game.event.get():
+            if event.type == pygame.quit():
+                sys.exit()
+        
+        pygame.display.flip()
+
+run_game()
