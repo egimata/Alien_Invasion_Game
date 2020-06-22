f7ffb21cb9e13a86af63f709bbc774f15b18c53d
Egi Mata <egi.mata1@gmail.com>
2020-06-22 19:28:45 +0200

    fixed play button fucntion making it not clickable in gamestart

--- game_functions.py
+++ game_functions.py
@@ -41,11 +41,12 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(ai_settings, stats, play_button, ship, aliens, bullets mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """start new game when clicking play"""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
         #resets game stats
         stats.reset_stats()
         stats.game_active = True
