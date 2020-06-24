b9d942e6f934917c23164d82f533dddc527db46f
Egi Mata <egi.mata1@gmail.com>
2020-06-24 15:00:39 +0200

    modified function for rounding numbers in scoreboard

--- settings.py
+++ settings.py
@@ -15,26 +15,30 @@ class Settings():
         self.bullet_height = 60
         self.bullet_color = (255, 51, 51)
         self.bullets_allowed = 7
-        self.alien_speed_factor = 1
-        self.fleet_drop_speed = 5
+        self.alien_speed_factor = 1.2
+        self.fleet_drop_speed = 6
         # 1 = right ; -1 = left
         self.fleet_direction = 1
         #game speed on lvlup
         self.speedup_scale = 1.3
+        #alien point value incr
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
     
     def initialize_dynamic_settings(self):
         """init settg that change throughout the game"""
         self.ship_speed_factor = 1
         self.bullet_speed_factor = 3
-        self.alien_speed_factor = 1
+        self.alien_speed_factor = 1.2
         self.fleet_direction = 1
         #scoring
         self.alien_points = 10
+    
 
     def increase_speed(self):
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
         
         
